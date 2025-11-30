57b9276bf22d677b5946dda25f4ef57482d3f4dc
Kaushal <kaushalpatel1208@gmail.com>
2025-11-30 14:41:27 +0530

    Initial Commit

---
+++ DemoWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,102 @@
+﻿using Demo.DataAccess.Data;
+using Demo.DataAccess.Repository.IRepository;
+using Demo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _unitOfWork.Category.GetAll().ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("", "Category Name and Display Order cannot be the same.");
+            }
+            if(ModelState.IsValid)
+            {
+                _unitOfWork.Category.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? record = _unitOfWork.Category.Get(u => u.ID == id);
+            if(record == null)
+            {
+                return NotFound();
+            }
+            return View(record);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("", "Category Name and Display Order cannot be the same.");
+            }
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Category.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? record = _unitOfWork.Category.Get(u => u.ID == id);
+            if (record == null)
+            {
+                return NotFound();
+            }
+            return View(record);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category? obj = _unitOfWork.Category.Get(u => u.ID == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Category.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+          }
+    }
+}
---
+++ DemoWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,102 @@
+﻿using Demo.DataAccess.Data;
+using Demo.DataAccess.Repository.IRepository;
+using Demo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+            return View(objProductList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product obj)
+        {
+            if (obj.Title == obj.ListPrice.ToString())
+            {
+                ModelState.AddModelError("", "Title and Price cannot be the same.");
+            }
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product listed successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+        Product? record = _unitOfWork.Product.Get(u => u.Id == id);
+            if(record == null)
+            {
+                return NotFound();
+             }
+            return View(record);
+         }
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+            if (obj.Title == obj.ListPrice.ToString())
+            {
+                ModelState.AddModelError("", "Title and Price cannot be the same.");
+            }
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product listing updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product? record = _unitOfWork.Product.Get(u => u.Id == id);
+            if (record == null)
+            {
+                return NotFound();
+            }
+            return View(record);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Product? obj = _unitOfWork.Product.Get(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Product.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Product listing deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
+
